a7ee35e3f0f4388762dd8e186d8c02304ab29ffe
haticenur <haticenur_dincel@hotmail.com>
2023-09-13 19:59:38 +0300

    indeks

---
+++ VucutKitleIndeksi/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int kilo;
+        double boy,indeks;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Lütfen Boyunuzu Metre Cinsinden Giriniz : ");
+        boy=input.nextDouble();
+        System.out.print("Lütfen Kilonuzu Giriniz : ");
+        kilo=input.nextInt();
+        indeks=kilo/(boy*boy);
+        System.out.println("Vücut Kitle İndeksiniz : "+indeks);
+    }
+}
