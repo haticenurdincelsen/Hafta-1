33032ead00f6b46d6db8e633b864ce022f28ca78
haticenur <haticenur_dincel@hotmail.com>
2023-09-15 04:28:53 +0300

    değer

--- CinZodyagi/src/Main.java
+++ CinZodyagi/src/Main.java
@@ -1,49 +1,26 @@
 import java.util.Scanner;
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        int yil,kalan;
-        Scanner input=new Scanner(System.in);
-        System.out.print("Dogum Yılınızı Giriniz : ");
-        yil=input.nextInt();
-        kalan=yil%12;
-        switch (kalan){
-            case 0:
-                System.out.print("Cin Zodyagı Burcunuz : Maymun ");
-                break;
-            case 1:
-                System.out.print("Cin Zodyagı Burcunuz : Horoz ");
-                break;
-            case 2:
-                System.out.print("Cin Zodyagı Burcunuz : Köpek ");
-                break;
-            case 3:
-                System.out.print("Cin Zodyagı Burcunuz : Domuz ");
-                break;
-            case 4:
-                System.out.print("Cin Zodyagı Burcunuz : Fare ");
-                break;
-            case 5:
-                System.out.print("Cin Zodyagı Burcunuz : Öküz ");
-                break;
-            case 6:
-                System.out.print("Cin Zodyagı Burcunuz : Kaplan ");
-                break;
-            case 7:
-                System.out.print("Cin Zodyagı Burcunuz : Tavşan ");
-                break;
-            case 8:
-                System.out.print("Cin Zodyagı Burcunuz : Ejderha ");
-                break;
-            case 9:
-                System.out.print("Cin Zodyagı Burcunuz : Yılan ");
-                break;
-            case 10:
-                System.out.print("Cin Zodyagı Burcunuz : At ");
-                break;
-            case 11:
-                System.out.print("Cin Zodyagı Burcunuz : Koyun ");
-                break;
-            default:
+        int birthYear;
+        System.out.print("Please enter your birth year: ");
+        Scanner input = new Scanner(System.in);
+        birthYear = input.nextInt();
+        int zodiac = birthYear % 12;
+        switch (zodiac) {
+            case 0 -> System.out.print("Your Chinese Zodiac is: Monkey");
+            case 1 -> System.out.print("Your Chinese Zodiac is: Rooster");
+            case 2 -> System.out.print("Your Chinese Zodiac is: Dog");
+            case 3 -> System.out.print("Your Chinese Zodiac is: Pig");
+            case 4 -> System.out.print("Your Chinese Zodiac is: Mouse");
+            case 5 -> System.out.print("Your Chinese Zodiac is: Ox");
+            case 6 -> System.out.print("Your Chinese Zodiac is: Tiger");
+            case 7 -> System.out.print("Your Chinese Zodiac is: Rabbit");
+            case 8 -> System.out.print("Your Chinese Zodiac is: Dragon");
+            case 9 -> System.out.print("Your Chinese Zodiac is: Snake");
+            case 10 -> System.out.print("Your Chinese Zodiac is: Horse");
+            case 11 -> System.out.print("Your Chinese Zodiac is: Sheep");
+            default -> System.out.print("Not Possible");
+            //Bu senaryoda input integer girildiği sürece default'a düşme imkanı yok!
         }
     }
 }
