39e27e389f815790d46d3cd23433c9168081a20b
haticenur <haticenur_dincel@hotmail.com>
2023-09-15 11:34:09 +0300

    deger

--- SinifiGecmeDurumu/src/Main.java
+++ SinifiGecmeDurumu/src/Main.java
@@ -3,17 +3,23 @@ public class Main {
     public static void main(String[] args) {
         int matematık, fizik, turkce, kimya, muzik;
         Scanner input = new Scanner(System.in);
+        //Kullanıcıdan verileri isteyelim.
         System.out.print(" Matematik Notunuzu Giriniz : ");
         matematık = input.nextInt();
+
         System.out.print(" Fizik Notunuzu Giriniz : ");
         fizik = input.nextInt();
+
         System.out.print(" Türkçe Notunuzu Giriniz :");
         turkce = input.nextInt();
+
         System.out.print(" Kimya Notunuzu Giriniz : ");
         kimya = input.nextInt();
+
         System.out.print(" Müzik Notunuzu Giriniz  :");
         muzik = input.nextInt();
-        if (matematık >= 0 && matematık <= 100 &&
+        // Koşulları belirleyelim.
+            if (matematık >= 0 && matematık <= 100 &&
                 fizik >= 0 && fizik <= 100 &&
                 turkce >= 0 && turkce <= 100 &&
                 kimya >= 0 && kimya <= 100 &&
@@ -24,12 +30,13 @@ public class Main {
             if (ortalama < 55) {
                 ;
                 System.out.println(" Sınıfta Kaldınız. ");
+
             } else {
                 System.out.println(" Sınıfı Geçtiniz. ");
             }
             System.out.println(" Ortalamanız : " + ortalama);
-
-        }else {
+            //0 ile 100 arasında not girilmediyse uyarı verecek koşulu girelim.
+            }else {
             System.out.println("Girilen Not Hatalıdır.Lütfen 0 ile 100 Arasında Not Girişi Yapınız.");
         }
     }
