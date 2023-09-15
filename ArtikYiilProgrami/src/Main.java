e301662855c1f6b91e5d7b74fd1fbe006a0e8246
haticenurdincelsen <142350794+haticenurdincelsen@users.noreply.github.com>
2023-09-15 09:47:25 +0300

    Add files via upload

---
+++ ArtikYiilProgrami/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int yıl;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Bir Yıl Giriniz ");
+        yıl =inp.nextInt();
+
+        boolean artıkyıl=false;
+        if(yıl %4 ==0) {
+            if (yıl % 100 != 0 || yıl % 400 == 0) {
+                artıkyıl = true;
+            }
+        }
+        if (artıkyıl) {
+            System.out.println(yıl + " Bir Artık Yıldır. ");
+        } else {
+            System.out.println(yıl + " Bir Artık Yıl Değildir. ");
+        }
+    }
+}
