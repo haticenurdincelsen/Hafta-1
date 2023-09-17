5227981c13ada7547d5e21d768a9f8abb461aaa7
haticenur <haticenur_dincel@hotmail.com>
2023-09-17 12:46:28 +0300

    deger

--- TersUcgenYapimi/src/Main.java
+++ TersUcgenYapimi/src/Main.java
@@ -7,12 +7,12 @@ public class Main {
         Ssayısı = input.nextInt();
         //Ters Üçgen yapımı
         for (int i=Ssayısı;i>0;i--){
-                for (int k = 0; k < Ssayısı - i; k++) {
-                    System.out.print(" ");
-                }
-                for (int m = 0; m < 2 * i- 1; m++) {
-                    System.out.print("*");
-                }
+            for (int k = 0; k < Ssayısı - i; k++) {
+                System.out.print(" ");
+            }
+            for (int m = 0; m < 2 * i- 1; m++) {
+                System.out.print("*");
+            }
             System.out.println();
         }
 
